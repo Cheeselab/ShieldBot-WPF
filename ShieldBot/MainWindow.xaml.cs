c45a5c5d2fc6eaa9d5699ac72a0c4509a5588ed5
Paladinleeds <daratcliffe1@outlook.com>
2016-08-23 12:46:54 +0100

    Phasing out old UI
    Just because Countdown had the best intro in 1998 (ref:
    https://www.youtube.com/watch?v=VQ67EjUHMXE), doesn't mean that the
    Windows 98 UI works for apps anymore. Started phasing out in favour of a
    more modern UI.

--- ShieldBot/MainWindow.xaml.cs
+++ ShieldBot/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ShieldBot
         {
             InitializeComponent();
         }
+
+        private void spamProtNav(object sender, RoutedEventArgs e)
+        {
+            currentContent.Content = new SpamProtection();
+        }
     }
 }
---
+++ ShieldBot/SpamProtection.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShieldBot
+{
+    /// <summary>
+    /// Interaction logic for SpamProtection.xaml
+    /// </summary>
+    public partial class SpamProtection : UserControl
+    {
+        public SpamProtection()
+        {
+            InitializeComponent();
+        }
+
+        private void spamSettingsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void whiteDeleteClick(object sender, RoutedEventArgs e)
+        {
+            while (whiteList.SelectedItems.Count > 0)
+            {
+                whiteList.Items.Remove(whiteList.SelectedItem);
+            }
+        }
+
+        private void whiteAddClick(object sender, RoutedEventArgs e)
+        {
+            string whiteText = whiteNew.Text.Trim();
+            if (String.IsNullOrEmpty(whiteNew.Text))
+            {
+                MessageBox.Show("Data empty, please add a URL");
+            }
+
+            else if (whiteList.Items.Contains(whiteText))
+            {
+                MessageBox.Show("Link already in list, can't add duplicate!");
+                whiteNew.Clear();
+            }
+
+            else
+            {
+                whiteList.Items.Add(whiteNew.Text);
+                whiteNew.Clear();
+            }
+        }
+
+        private void blackDeleteClick(object sender, RoutedEventArgs e)
+        {
+            while (blackList.SelectedItems.Count > 0)
+            {
+                blackList.Items.Remove(blackList.SelectedItem);
+            }
+        }
+
+        private void blackAddClick(object sender, RoutedEventArgs e)
+        {
+            string blackText = blackNew.Text.Trim();
+            if (String.IsNullOrEmpty(blackNew.Text))
+            {
+                MessageBox.Show("No entry detected. Please try again.");
+            }
+
+            else if (blackList.Items.Contains(blackText))
+            {
+                MessageBox.Show("Blacklisted item already in list, can't add duplicate!");
+                blackNew.Clear();
+            }
+
+            else
+            {
+                blackList.Items.Add(blackNew.Text);
+                blackNew.Clear();
+            }
+        }
+
+        private void nameDeleteClick(object sender, RoutedEventArgs e)
+        {
+            while (nameList.SelectedItems.Count > 0)
+            {
+                nameList.Items.Remove(nameList.SelectedItem);
+            }
+        }
+
+        public void nameAddClick(object sender, RoutedEventArgs e)
+        {
+            string nameText = nameNew.Text.Trim();
+            if (String.IsNullOrEmpty(blackNew.Text))
+            {
+                MessageBox.Show("No entry detected. Please try again.");
+            }
+
+            else if (nameList.Items.Contains(nameText))
+            {
+                MessageBox.Show("Blacklisted item already in list, can't add duplicate!");
+                nameNew.Clear();
+            }
+
+            else
+            {
+                nameList.Items.Add(nameNew.Text);
+                nameNew.Clear();
+            }
+        }
+    }
+}
